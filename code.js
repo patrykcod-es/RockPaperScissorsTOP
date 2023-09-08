7e85108ee6a1670f9c8ba6442cfbeeab6bc7cff4
Patryk Gaworski <patryk.gaworski@gmail.com>
2023-09-08 20:43:41 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,15 @@
 console.log("rock paper scissors");
 
 const choices = ["rock", "paper", "scissors"];
+<<<<<<< Updated upstream
+=======
+const buttons = document.querySelectorAll("button");
+const playerPointsUI = document.querySelector(".playerPoints");
+const computerPointsUI = document.querySelector(".computerPoints");
+const result = document.createElement("p");
+const pointsContainer = document.querySelector(".points");
+
+>>>>>>> Stashed changes
 let playerPoints = 0;
 let computerPoints = 0;
 function getRandomInt(x) {
@@ -20,13 +29,18 @@ function playRound(playerSelection, computerSelection) {
     (player === "scissors" && computerSelection === "paper")
   ) {
     playerPoints += 1;
+<<<<<<< Updated upstream
     console.log(`You won ${player} beats ${computerSelection}`);
+=======
+    result.textContent = `You won ${player} beats ${computerSelection}`;
+>>>>>>> Stashed changes
   } else if (
     (player === "rock" && computerSelection === "paper") ||
     (player === "paper" && computerSelection === "scissors") ||
     (player === "scissors" && computerSelection === "rock")
   ) {
     computerPoints += 1;
+<<<<<<< Updated upstream
     console.log(`You lose! ${computerSelection} beats ${player}`);
   } else {
     console.log(`You choose ${player} together`);
@@ -47,6 +61,35 @@ function game() {
     console.log(
       `There is no winner, only loosers .You both got ${playerPoints} points`
     );
+=======
+    result.textContent = `You lose! ${computerSelection} beats ${player}`;
+  } else {
+    result.textContent = `You choose ${player} together`;
+  }
+}
+function gameOver() {
+  if (round >= 5) {
+    buttons.forEach((button) => button.setAttribute("disabled", true));
+  }
+}
+
+function game(e) {
+  pointsContainer.appendChild(result);
+  let playerChoice = e.target.id;
+  let computerChoice = getComputerChoice();
+  playRound(playerChoice, computerChoice);
+  if (round >= 5) {
+    if (playerPoints > computerPoints) {
+      gameOver();
+      result.textContent = `Well done you won ${playerPoints} to ${computerPoints}`;
+    } else if (playerPoints < computerPoints) {
+      gameOver();
+      result.textContent = `You loose ${computerPoints} to ${playerPoints}`;
+    } else {
+      gameOver();
+      result.textContent = `There is no winner, only loosers .You both got ${playerPoints} points`;
+    }
+>>>>>>> Stashed changes
   }
 }
 function repeatString(string, times) {
