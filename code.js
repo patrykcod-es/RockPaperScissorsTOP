e2eb7f9c6d773601c4a946b2ba925515774ad1d7
Patryk Gaworski <patryk.gaworski@gmail.com>
2023-09-08 20:59:22 +0200

    finished game logic and VERY basic UI
    finished game logic, game disable buttons after 5 rounds and display points and results of the round on the screen

--- code.js
+++ code.js
@@ -1,17 +1,21 @@
 console.log("rock paper scissors");
 
+// variables
+
 const choices = ["rock", "paper", "scissors"];
-<<<<<<< Updated upstream
-=======
 const buttons = document.querySelectorAll("button");
 const playerPointsUI = document.querySelector(".playerPoints");
 const computerPointsUI = document.querySelector(".computerPoints");
 const result = document.createElement("p");
 const pointsContainer = document.querySelector(".points");
 
->>>>>>> Stashed changes
 let playerPoints = 0;
 let computerPoints = 0;
+let playerChoice = "";
+let round = 0;
+
+// helping function
+
 function getRandomInt(x) {
   return Math.floor(Math.random() * x);
 }
@@ -23,45 +27,20 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   const player = playerSelection.toLowerCase();
 
+function playRound(player, computerSelection) {
   if (
     (player === "rock" && computerSelection === "scissors") ||
     (player === "paper" && computerSelection === "rock") ||
     (player === "scissors" && computerSelection === "paper")
   ) {
     playerPoints += 1;
-<<<<<<< Updated upstream
-    console.log(`You won ${player} beats ${computerSelection}`);
-=======
     result.textContent = `You won ${player} beats ${computerSelection}`;
->>>>>>> Stashed changes
   } else if (
     (player === "rock" && computerSelection === "paper") ||
     (player === "paper" && computerSelection === "scissors") ||
     (player === "scissors" && computerSelection === "rock")
   ) {
     computerPoints += 1;
-<<<<<<< Updated upstream
-    console.log(`You lose! ${computerSelection} beats ${player}`);
-  } else {
-    console.log(`You choose ${player} together`);
-  }
-}
-
-function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerChoice = prompt("Rock, paper or scissor?");
-    let computerChoice = getComputerChoice();
-    playRound(playerChoice, computerChoice);
-  }
-  if (playerPoints > computerPoints) {
-    console.log(`Well done you won ${playerPoints} to ${computerPoints}`);
-  } else if (playerPoints < computerPoints) {
-    console.log(`You loose ${computerPoints} to ${playerPoints}`);
-  } else {
-    console.log(
-      `There is no winner, only loosers .You both got ${playerPoints} points`
-    );
-=======
     result.textContent = `You lose! ${computerSelection} beats ${player}`;
   } else {
     result.textContent = `You choose ${player} together`;
@@ -89,102 +68,12 @@ function game(e) {
       gameOver();
       result.textContent = `There is no winner, only loosers .You both got ${playerPoints} points`;
     }
->>>>>>> Stashed changes
   }
+  round += 1;
+  playerPointsUI.textContent = playerPoints;
+  computerPointsUI.textContent = computerPoints;
 }
-function repeatString(string, times) {
-  let repeatedtext = "";
-  for (let i = 0; i < times; i++) {
-    repeatedtext += string;
-  }
-  console.log(repeatedtext);
-}
-repeatString("hey", 3);
-function reverseString(string) {
-  let arr = string.split("");
-  let revArr = arr.reverse();
-  let revStr = revArr.join("");
-
-  console.log(revStr);
-}
-reverseString("jebacpis");
-
-function sumAll(start, finish) {
-  let sum = 0;
-  for (let i = start; i <= finish; i++) {
-    sum += i;
-    console.log(sum);
-  }
-}
-sumAll(1, 4);
-
-function lapYears(year) {
-  if (year % 100 === 0 && year % 400 !== 0) {
-    console.log("false");
-    return false;
-  } else if (year % 4 === 0 || year % 400 === 0) {
-    console.log("true");
-    return true;
-  } else {
-    console.log("false");
-    return false;
-  }
-}
-lapYears(2000);
-
-function convertCelcToFaren(temp) {
-  let faren = temp * 1.8 + 32;
-  let shortFaren = Number.parseFloat(faren).toFixed(1);
-  console.log(shortFaren);
-}
-convertCelcToFaren(37.6);
-function convertFarenToCelc(temp) {
-  let celc = (temp - 32) * (5 / 9);
-  let shortCelc = Number.parseFloat(celc).toFixed(1);
-  console.log(shortCelc);
-}
-convertFarenToCelc(100);
-
-// a <p> with red text that says “Hey I’m red!”
-// an <h3> with blue text that says “I’m a blue h3!”
-// a <div> with a black border and pink background color with the following elements inside of it:
-//     another <h1> that says “I’m in a div”
-//     a <p> that says “ME TOO!”
-//     Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
-
-const container = document.querySelector("#container");
-const paragraph = document.createElement("p");
-paragraph.textContent = "Hey I'm red";
-paragraph.setAttribute("style", "color: red");
-container.appendChild(paragraph);
-
-const header = document.createElement("h3");
-header.textContent = `I'm blue H3`;
-header.setAttribute("style", "color: blue");
-container.appendChild(header);
-
-const div = document.createElement("div");
-div.setAttribute("style", "border: 1px solid black; backgroundColor: pink");
-container.appendChild(div);
-
-const mainHeader = document.createElement("h1");
-mainHeader.textContent = `I'm in a div`;
-
-div.appendChild(mainHeader);
-
-const divParagraph = document.createElement("p");
-divParagraph.textContent = "ME TOO";
-div.appendChild(divParagraph);
-
-const btn = document.getElementById("btn");
-btn.addEventListener("click", (e) => {
-  e.target.style.background = "blue";
-});
-
-const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    console.log(button.id);
-  });
+  button.addEventListener("click", (e) => game(e));
 });
