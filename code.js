ca8c43ea3dd55c5c46b915f2fbec3cce3be57f94
Patryk Gaworski <patryk.gaworski@gmail.com>
2023-09-08 17:36:57 +0200

    exercises of TOP fundation, not related to the game

--- code.js
+++ code.js
@@ -49,4 +49,99 @@ function game() {
     );
   }
 }
-game();
+function repeatString(string, times) {
+  let repeatedtext = "";
+  for (let i = 0; i < times; i++) {
+    repeatedtext += string;
+  }
+  console.log(repeatedtext);
+}
+repeatString("hey", 3);
+function reverseString(string) {
+  let arr = string.split("");
+  let revArr = arr.reverse();
+  let revStr = revArr.join("");
+
+  console.log(revStr);
+}
+reverseString("jebacpis");
+
+function sumAll(start, finish) {
+  let sum = 0;
+  for (let i = start; i <= finish; i++) {
+    sum += i;
+    console.log(sum);
+  }
+}
+sumAll(1, 4);
+
+function lapYears(year) {
+  if (year % 100 === 0 && year % 400 !== 0) {
+    console.log("false");
+    return false;
+  } else if (year % 4 === 0 || year % 400 === 0) {
+    console.log("true");
+    return true;
+  } else {
+    console.log("false");
+    return false;
+  }
+}
+lapYears(2000);
+
+function convertCelcToFaren(temp) {
+  let faren = temp * 1.8 + 32;
+  let shortFaren = Number.parseFloat(faren).toFixed(1);
+  console.log(shortFaren);
+}
+convertCelcToFaren(37.6);
+function convertFarenToCelc(temp) {
+  let celc = (temp - 32) * (5 / 9);
+  let shortCelc = Number.parseFloat(celc).toFixed(1);
+  console.log(shortCelc);
+}
+convertFarenToCelc(100);
+
+// a <p> with red text that says “Hey I’m red!”
+// an <h3> with blue text that says “I’m a blue h3!”
+// a <div> with a black border and pink background color with the following elements inside of it:
+//     another <h1> that says “I’m in a div”
+//     a <p> that says “ME TOO!”
+//     Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
+
+const container = document.querySelector("#container");
+const paragraph = document.createElement("p");
+paragraph.textContent = "Hey I'm red";
+paragraph.setAttribute("style", "color: red");
+container.appendChild(paragraph);
+
+const header = document.createElement("h3");
+header.textContent = `I'm blue H3`;
+header.setAttribute("style", "color: blue");
+container.appendChild(header);
+
+const div = document.createElement("div");
+div.setAttribute("style", "border: 1px solid black; backgroundColor: pink");
+container.appendChild(div);
+
+const mainHeader = document.createElement("h1");
+mainHeader.textContent = `I'm in a div`;
+
+div.appendChild(mainHeader);
+
+const divParagraph = document.createElement("p");
+divParagraph.textContent = "ME TOO";
+div.appendChild(divParagraph);
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click", (e) => {
+  e.target.style.background = "blue";
+});
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.id);
+  });
+});
