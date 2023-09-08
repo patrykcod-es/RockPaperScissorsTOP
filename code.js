6ddb2c45c0408968e983fc5f44ae6a905b24571f
Patryk Gaworski <patryk.gaworski@gmail.com>
2023-09-08 20:38:14 +0200

    game logic and VERY basic UI done
    finished the game, results are displayed on the page and buttons get disabled after 5 rounds

--- code.js
+++ code.js
@@ -1,8 +1,21 @@
 console.log("rock paper scissors");
 
+// variables
+
 const choices = ["rock", "paper", "scissors"];
+const buttons = document.querySelectorAll("button");
+const playerPointsUI = document.querySelector(".playerPoints");
+const computerPointsUI = document.querySelector(".computerPoints");
+const result = document.createElement('p')
+const pointsContainer = document.querySelector('.points')
+
 let playerPoints = 0;
 let computerPoints = 0;
+let playerChoice = "";
+let round = 0;
+
+// helping function
+
 function getRandomInt(x) {
   return Math.floor(Math.random() * x);
 }
@@ -11,42 +24,64 @@ function getComputerChoice() {
   return choices[choice];
 }
 
-function playRound(playerSelection, computerSelection) {
-  const player = playerSelection.toLowerCase();
+// function getPlayerChoice(e) {
+//     playerChoice = e.target.id
+// }
+// buttons.forEach((button) => {
+//     button.addEventListener('click', getPlayerChoice);
+// });
 
+// game logic
+
+function playRound(player, computerSelection) {
   if (
     (player === "rock" && computerSelection === "scissors") ||
     (player === "paper" && computerSelection === "rock") ||
     (player === "scissors" && computerSelection === "paper")
   ) {
     playerPoints += 1;
-    console.log(`You won ${player} beats ${computerSelection}`);
+    result.textContent = `You won ${player} beats ${computerSelection}`
   } else if (
     (player === "rock" && computerSelection === "paper") ||
     (player === "paper" && computerSelection === "scissors") ||
     (player === "scissors" && computerSelection === "rock")
   ) {
     computerPoints += 1;
-    console.log(`You lose! ${computerSelection} beats ${player}`);
+    result.textContent = `You lose! ${computerSelection} beats ${player}`
   } else {
-    console.log(`You choose ${player} together`);
+    result.textContent = `You choose ${player} together`
+  }
+}
+function gameOver() {
+  if (round >= 5) {
+    buttons.forEach((button) => button.setAttribute("disabled", true));
   }
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerChoice = prompt("Rock, paper or scissor?");
-    let computerChoice = getComputerChoice();
-    playRound(playerChoice, computerChoice);
+function game(e) {
+    pointsContainer.appendChild(result)
+  let playerChoice = e.target.id;
+  let computerChoice = getComputerChoice();
+  playRound(playerChoice, computerChoice);
+  if (round >= 5) {
+    if (playerPoints > computerPoints) {
+      gameOver();
+      result.textContent = `Well done you won ${playerPoints} to ${computerPoints}`
+    } else if (playerPoints < computerPoints) {
+      gameOver();
+      result.textContent = `You loose ${computerPoints} to ${playerPoints}`
+    } else {
+      gameOver();
+      result.textContent = 
+        `There is no winner, only loosers .You both got ${playerPoints} points`
+      
+    }
   }
-  if (playerPoints > computerPoints) {
-    console.log(`Well done you won ${playerPoints} to ${computerPoints}`);
-  } else if (playerPoints < computerPoints) {
-    console.log(`You loose ${computerPoints} to ${playerPoints}`);
-  } else {
-    console.log(
-      `There is no winner, only loosers .You both got ${playerPoints} points`
-    );
-  }
+  round += 1;
+  playerPointsUI.textContent = playerPoints;
+  computerPointsUI.textContent = computerPoints;
 }
-game();
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => game(e));
+});
